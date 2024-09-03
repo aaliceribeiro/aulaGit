f54fc091e93d4341c2974c3b120c795916597d8d
aaliceribeiro <silveirar.alice@gmail.com>
2024-09-03 17:05:14 -0300

    criei o arquivo media.js

---
+++ media.js
@@ -0,0 +1,6 @@
+let n1 = Number (prompt('Digite o primeiro numero: '))
+let n2 = Number (prompt('Digite o segundo numero: '))
+let n3 = Number (prompt('Digite o terceiro numero: '))
+const media = (idade1 + idade2 + idade3)  / 3
+
+console.log(media)
